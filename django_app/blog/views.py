ab51aadaf8853552f4eb481901faa57f8dec9082
Jorge Aldair Cortes Lopez <jorge.cortes@ift.org.mx>
2021-08-11 15:20:11 -0500

    decoupling django key

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -3,10 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 
 
+posts = [
+    {
+        'author' : 'Corey S',
+        'title' :'Blog post 1',
+        'content' : 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author' : 'Jane Doe',
+        'title' :'Blog post 2',
+        'content' : 'Seconf post content',
+        'date_posted': 'August 47, 2018'
+    }
+]
+
 def home (request):
-    return render(request,'blog/home.html')
+
+    context = {
+        'posts' : posts
+    }
+
+    return render(request,'blog/home.html',context)
 
 
 def about (request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html', {'title':'About'})
 
--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path\
+from decouple import config    
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x-b2rmg2p7h%_i^i72iwq4z+7+*#gns9c08deau29r#v3c!bt#'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
