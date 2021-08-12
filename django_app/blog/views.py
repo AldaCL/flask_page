f03364f210cad73dbccbf9607991ed67dc90fd6e
Jorge Aldair Cortes Lopez <jorge.cortes@ift.org.mx>
2021-08-12 17:59:56 -0500

    Haciendo el Login

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,27 +1,11 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-# Create your views here.
+from .models import Post
 
-
-posts = [
-    {
-        'author' : 'Corey S',
-        'title' :'Blog post 1',
-        'content' : 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' :'Blog post 2',
-        'content' : 'Seconf post content',
-        'date_posted': 'August 47, 2018'
-    }
-]
 
 def home (request):
 
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
 
     return render(request,'blog/home.html',context)
--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path\
-from decouple import config    
-
+from pathlib import Path
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
 
 ]
 
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
